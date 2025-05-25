10dca9176e845be6b7afcf7f3c1f6872061bece9
dazhonmiller1 <72367721+dazhonmiller1@users.noreply.github.com>
2025-05-25 13:08:51 -0400

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -27,7 +29,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
+  }
 )
 
 export interface ButtonProps
@@ -40,7 +42,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
-  },
+  }
 )
 Button.displayName = "Button"
 
